f438603c0c82458eca2db077d0ca143d87f7ae12
1368833931@qq.com <1368833931@qq.com>
2019-02-24 16:22:59 +0800

    create project

---
+++ src/main/java/com/personal/yvan/literatureforum/LiteratureforumApplication.java
@@ -0,0 +1,13 @@
+package com.personal.yvan.literatureforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratureforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteratureforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/yvan/literatureforum/LiteratureforumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.personal.yvan.literatureforum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiteratureforumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
